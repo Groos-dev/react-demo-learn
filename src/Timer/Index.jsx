8e968939fd40a2db41b77676ff560007eae7a828
groos <gegu926@gmial.com>
2024-10-14 23:58:42 +0800

    fix: fix incorrect stop timmer, but memory leaks may occur

--- src/Timer/Index.jsx
+++ src/Timer/Index.jsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function Timer() {
     const [time, setTime] = useState(new Date().getTime());
-
-    const timer = setInterval(() => {
-        setTime(new Date().getTime());
-    }, 1000);
-    function cancelTimer() {
-        clearInterval(timer);
+    const timer = useRef(null);
+    function startTimer() {
+        timer.current = setInterval(() => {
+            setTime(new Date().getTime());
+        }, 1000);
     }
-
+    function cancelTimer() {
+        clearInterval(timer.current);
+    }
     return (
         <>
-            <div>{time}</div>
+            <div>{new Date().getTime()}</div>
             <button onClick={cancelTimer}>stop</button>
+            <button onClick={startTimer}>start</button>
         </>
     );
 }
